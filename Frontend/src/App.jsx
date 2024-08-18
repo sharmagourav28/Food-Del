666757e70991ff7edd66a3597c68142e6f9f29ac
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-18 09:42:42 +0530

    Footer Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,21 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Placeorder from "./pages/Placeorder/Placeorder";
 import Cart from "./pages/Cart/Cart";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Cart />} />
-        <Route path="/" element={<Placeorder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Cart />} />
+          <Route path="/" element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 export default App;
