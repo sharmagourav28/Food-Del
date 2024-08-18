fe6abe1c7937a91e2c54a0a0f193260b31357751
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-18 10:09:00 +0530

    AppDownload done

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
   const [category, setcategory] = useState("All");
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setcategory={setcategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
