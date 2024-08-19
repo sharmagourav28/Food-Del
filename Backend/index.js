645fbfbb0cc06376093bfa51e2b8372b1c229f25
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-19 20:41:24 +0530

    Done api worked

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
-
 import connectDB from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 const app = express();
 const PORT = 7000;
 
 app.use(express.json());
 connectDB();
+
+// api end point
+app.use("/api/food", foodRouter);
 
 app.get("/", (req, res) => {
   res.send("APi working FoodDel");
