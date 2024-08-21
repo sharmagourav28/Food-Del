8782d58f4ad4ecb173f954f579c1112f8adff234
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-21 12:50:11 +0530

    login / resgister api works

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import connectDB from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import cors from "cors";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
 
 const app = express();
 const PORT = 7000;
@@ -14,6 +16,8 @@ connectDB();
 app.use("/api/food", foodRouter);
 
 app.use("/images", express.static("uploads"));
+
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("APi working FoodDel");
